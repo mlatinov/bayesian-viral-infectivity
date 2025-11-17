377ffb96e533468451805b23e8a471e69ceab804
mlatinov <latinovmetodi@gmail.com>
2025-11-18 00:18:44 +0300

    Add new data and a second model trained on bolt experimental datasets

--- _targets.R
+++ _targets.R
@@ -3,7 +3,6 @@
 library(targets)
 library(tidyverse)
 
-
 #### Source Function ####
 tar_source("scripts/")
 
@@ -13,20 +12,31 @@ list(
 
   #### Load the Experimental Data ####
   tar_target(
-    name  = data_raw,
-    command =  read_csv("data/Virus_herpes_Exp1 - Virus_data_1.csv")
+    name = experimental_data,
+    command = load_experiment()
   ),
 
   #### Clean the Raw Data ####
   tar_target(
-    name = data_clean,
-    command = clean_data_f(data_raw)
+    name = clean_experimental_data,
+    command = lapply(experimental_data, clean_data_f)
+  ),
+
+  ## Combine the clean datasets
+  tar_target(
+    name = data_combined,
+    command = bind_rows(
+      as.data.frame(clean_experimental_data[[1]]),
+      as.data.frame(clean_experimental_data[[1]])
+      )
   ),
 
   #### Classic GLM ####
   tar_target(
     name = glm_model,
-    command = glm_modeling_f(data_clean)
+    command = glm_modeling_f(
+      data = as.data.frame(clean_experimental_data[[1]])
+      )
   ),
 
   #### Bayes Models #####
@@ -35,7 +45,7 @@ list(
   tar_target(
     name = pp_sim_bernoulli,
     command = pp_simualtion_bernoulli(
-      data = data_clean,
+      data = as.data.frame(clean_experimental_data[[1]]),
       priors = c(
         # For each increase of log(virus_dilution) the chance of virus infection drops
         prior(normal(0, 3), class = "b", coef = "virus_dilution"),
@@ -50,29 +60,73 @@ list(
     command = inspect_simulation(pp_sim_bernoulli)
   ),
 
-  #### Bernoulli Bayes Model ####
+  #### Bernoulli Bayes Model with Experimental data 1 ####
   tar_target(
     name = bernoulli_bayes_model,
     command = bernoulli_bayes(
-      data = data_clean,
+      data = as.data.frame(clean_experimental_data[[1]]),
       priors = c(
         # For each increase of log(virus_dilution) the chance of virus infection drops by
         prior(normal(0, 3), class = "b", coef = "virus_dilution"),
         # When virus_dilution = 0 The log odds of virus infection
         prior(normal(2,2),class = "Intercept")
-      ))
+        )
+      )
   ),
 
-  ## Model Diagnostics ##
+  #### Bernoulli Bayes Model with Combined datasets ####
+  tar_target(
+    name = bernoulli_bayes_model_v2,
+    command = bernoulli_bayes(
+      data = data_combined,
+      priors = c(
+        # For each increase of log(virus_dilution) the chance of virus infection drops by
+        prior(normal(0, 3), class = "b", coef = "virus_dilution"),
+        # When virus_dilution = 0 The log odds of virus infection
+        prior(normal(2,2),class = "Intercept")
+        )
+      )
+  ),
+
+  ## Model Diagnostics for Bayes Model trained on Experimental data 1 ##
   tar_target(
     name = bernoulli_model_diagnostics,
     command = bayes_diagnostics(bernoulli_bayes_model)
   ),
 
-  ## Model Insights ##
+  ## Model Diagnostics for Bayes Model trained Combined data ##
+  tar_target(
+    name = bernoulli_model_diagnostics_2,
+    command = bayes_diagnostics(bernoulli_bayes_model_v2)
+  ),
+
+  ## Model Insights for Bayes Model trained on Experimental data 1 ##
   tar_target(
     name = bernoulli_model_insights,
-    command = bayes_insights(bernoulli_bayes_model)
+    command = bayes_insights(
+      model = bernoulli_bayes_model,
+      data = as.data.frame(clean_experimental_data[[1]])
+      )
+  ),
+
+  ## Model Insights for Bayes Model trained Combined data  ##
+  tar_target(
+    name = bernoulli_model_insights_2,
+    command = bayes_insights(
+      model = bernoulli_bayes_model_v2,
+      data = data_combined
+      )
+  ),
+
+  #### Model Compare ####
+  tar_target(
+    name = model_compare,
+    command = compare_bayes_models(
+      model_1_diag = bernoulli_model_diagnostics,
+      model_2_diag = bernoulli_model_diagnostics_2,
+      insights_1 = bernoulli_model_insights,
+      insights_2 = bernoulli_model_insights_2
+      )
   )
 )
 
