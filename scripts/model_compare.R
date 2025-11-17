92294693f6c66b8359b00587ad65c1698f5cab1d
mlatinov <latinovmetodi@gmail.com>
2025-11-18 00:22:25 +0300

    An idea for a function to compare and combine plots generated by the diagnostics and insights, the results will be used for future documents

---
+++ scripts/model_compare.R
@@ -0,0 +1,28 @@
+
+#### Function to Compare Bayesian Models ####
+compare_bayes_models <- function(model_1_diag,model_2_diag,insights_1,insights_2) {
+
+
+  #### Compare Diagnostics ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # Return
+  return(list(model_1_diag,model_2_diag,insights_1,insights_2))
+}
