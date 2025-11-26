3b4241be1d56bf17f5f99681d8c7d9f9daafebc9
mlatinov <latinovmetodi@gmail.com>
2025-11-27 01:17:25 +0300

    Outline the SBC protocol

---
+++ scripts/sbc_combined.R
@@ -0,0 +1,24 @@
+
+#### Function for SBC on the Combined Model ####
+sbc_combined <- function(){
+
+  #### Libraries ####
+  library(SBC)
+
+  #### Generator ####
+
+  #### Create SBC Datasets from generator ####
+
+  #### Compute SBC ####
+  sbc <- compute_SBC(datasets = ,)
+
+
+
+
+
+
+
+
+
+
+}
---
+++ scripts/sbc_hierarchical.R
@@ -0,0 +1,15 @@
+#### Function for SBC on Hierarchical model ####
+sbc_hierarchical <- function(){
+
+  #### Libraries ####
+  library(SBC)
+
+  #### Generator ####
+
+  #### Create SBC Datasets from generator ####
+
+  #### Compute SBC ####
+  sbc <- compute_SBC(datasets = ,)
+
+
+}
