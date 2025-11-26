ef857c16d8738376492adaf78aa15c518d75caf9
mlatinov <latinovmetodi@gmail.com>
2025-11-27 01:17:54 +0300

    Replace the fixed numbers with draw variable

--- scripts/simulate_virus.R
+++ scripts/simulate_virus.R
@@ -13,10 +13,10 @@ simulate_virus <- function(
   ## The Parameters for the P_i equation comes from normal distribution ##
 
   # Intercept The probability of infection when x_i = 0
-  a <- rnorm(n = 10, mean = baseline_infectivity_mean,baseline_infectivity_sd)
+  a <- rnorm(n = n_draws, mean = baseline_infectivity_mean,baseline_infectivity_sd)
 
   # Coef for the impact of the dilution on the probability of infection
-  b <- rnorm(n = 10, mean = beta_coef_mean,beta_coef_sd)
+  b <- rnorm(n = n_draws, mean = beta_coef_mean,beta_coef_sd)
 
   ## The Parameter P_i can be expressed as logistic linear equation
   p_matrix <- sapply(1:n_draws, function(i) 1 / (1 + exp(-(a[i] + b[i] * x_i))))
