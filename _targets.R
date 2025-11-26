50f35f9a0ceeefb1fe55499f6088e7654b1cf98c
mlatinov <latinovmetodi@gmail.com>
2025-11-27 01:16:22 +0300

    Add Hierarchical model and SBC protocol

--- _targets.R
+++ _targets.R
@@ -48,6 +48,20 @@ list(
     )
   ),
 
+  # Simulate data for Hierarchical Virus Model with Multiple Repeated Experiments
+  tar_target(
+    name = sim_data_hierarchical,
+    command = simulate_virus_hierarchical(
+      baseline_infectivity_mean = 5,
+      baseline_infectivity_sd = 0.5,
+      beta_coef_mean = 1.2 ,
+      beta_coef_sd = 0.15,
+      rho = 0.8,                   # correlation between intercepts and slopes
+      n_experiments = 5,           # number of experiments
+      n_per_experiment = 500       # number of dilution points per experiment
+    )
+  ),
+
   ## Combine the datasets in a list ##
   tar_target(
     name = simulation_virus_AB,
@@ -57,7 +71,7 @@ list(
     )
   ),
 
-  ## Check Generated data
+  #### Check Generated data ####
 
   # Check the AB Viruses
   tar_target(
@@ -69,6 +83,12 @@ list(
   tar_target(
     name = simulation_data_check_G,
     command = check_generated_virus(virus_G)
+  ),
+
+  # Check Hierarchical Virus Data
+  tar_target(
+    name = simulation_data_check_hierarchical,
+    command = check_generated_virus(sim_data_hierarchical)
   ),
 
   #### Load the Experimental Datasets ####
@@ -88,13 +108,17 @@ list(
     name = data_combined,
     command = bind_rows(
       as.data.frame(clean_experimental_data[[1]]),
-      as.data.frame(clean_experimental_data[[1]])
+      as.data.frame(clean_experimental_data[[1]]),
+      .id = "experiment"
+      ) %>%
+      mutate(
+        experiment = as.factor(experiment)
       )
   ),
 
-  #### Bayes Models #####
+  #### Bayes Models Parameter Recovery #####
 
-  #### Bernoulli Bayes Model Simulation Virus AB ####
+  # Bernoulli Bayes Model Simulation Virus AB #
   tar_target(
     name = bernoulli_bayes_model_sim_virus_AB,
     command = lapply(
@@ -111,7 +135,7 @@ list(
     )
   ),
 
-  #### Bernoulli Bayes Model Simulation Virus G ####
+  # Bernoulli Bayes Model Simulation Virus G #
   tar_target(
     name = bernoulli_bayes_model_sim_virus_G,
     command = bernoulli_bayes_G(
@@ -124,7 +148,23 @@ list(
     )
   ),
 
-  ### Prior Predictive Simulation for the Experimental Data  ###
+  # Bernoulli Bayes Model for Hierarchical Model
+  tar_target(
+    name = bernoulli_sim_hierarchical,
+    command = bernoulli_hierarchical(
+      data = sim_data_hierarchical,
+      priors = c(
+        prior(normal(0,3), class = "b"),                   # slope coefficients
+        prior(normal(2,2), class = "Intercept"),           # population intercept
+        prior(exponential(2), class = "sd"),               # group-level SD
+        prior(lkj(0.5), class = "cor", group = "experiment")  # correlation
+      )
+    )
+  ),
+
+  #### Prior Predictive Simulations for the Real Models ####
+
+  # Prior Predictive Simulation for the Experimental Data  #
   tar_target(
     name = pp_sim_bernoulli,
     command = pp_simualtion_bernoulli(
@@ -137,13 +177,52 @@ list(
       ))
   ),
 
-  ## Inspect Prior Predictive Simulation model for the Experimental Data ##
+  # Prior Predictive Simulation on the Combined Data for Hierarchical Model
   tar_target(
-    name = pp_simualtion_inspections,
-    command = inspect_simulation(pp_sim_bernoulli)
+    name = pp_sim_bernoulli_hierarchical,
+    command = pp_simulation_bernoulli_hierarchical(
+      data = sim_data_hierarchical,
+      priors = c(
+        prior(normal(0,3), class = "b"),                   # slope coefficients
+        prior(normal(2,2), class = "Intercept"),           # population intercept
+        prior(exponential(2), class = "sd"),               # group-level SD
+        prior(lkj(0.5), class = "cor", group = "experiment")  # correlation
+        )
+      )
+    ),
+
+  # Collect all PP Simulations in a list
+  tar_target(
+    name = pp_sim_list,
+    command = list(
+      pp_simulation_data_1 = pp_sim_bernoulli,
+      pp_simulation_hierarchical = pp_sim_bernoulli_hierarchical
+    )
   ),
 
-  #### Bernoulli Bayes Model with Experimental data 1 ####
+  ## Inspect Prior Predictive Simulation models ##
+  tar_target(
+    name = pp_simualtion_inspections,
+    command = lapply(pp_sim_list,inspect_simulation)
+  ),
+
+  #### Bayes Models Simulation Based Calibration #####
+
+  # SBC For Combined Data Bayes Model
+  tar_target(
+    name = sbc_bernoulli_combined,
+    command = sbc_combined()
+  ),
+
+  # SBC For Hierarchical Bayes Model
+  tar_target(
+    name = sbc_bernoulli_hierarchical,
+    command = sbc_hierarchical()
+  ),
+
+  #### Full Bayesian Models ####
+
+  # Bernoulli Bayes Model with Experimental data 1 #
   tar_target(
     name = bernoulli_bayes_model,
     command = bernoulli_bayes(
@@ -157,7 +236,7 @@ list(
       )
   ),
 
-  #### Bernoulli Bayes Model with Combined Experimental datasets ####
+  # Bernoulli Bayes Model with Combined Experimental datasets #
   tar_target(
     name = bernoulli_bayes_model_v2,
     command = bernoulli_bayes(
@@ -171,25 +250,42 @@ list(
       )
   ),
 
-  #### Append All the models in one list ####
+  # Bernoulli Hierarchical Model for the Different Experiments #
+  tar_target(
+    name = bernoulli_hierarchical_model,
+    command = bernoulli_hierarchical(
+      data = data_combined,
+      priors = c(
+        prior(normal(0,3), class = "b"),                   # slope coefficients
+        prior(normal(2,2), class = "Intercept"),           # population intercept
+        prior(exponential(2), class = "sd"),               # group-level SD
+        prior(lkj(0.5), class = "cor", group = "experiment")  # correlation
+      )
+    )
+  ),
+
+  # Append All the models in one list #
   tar_target(
     name = all_models,
     command = append(
       bernoulli_bayes_model_sim_virus_AB,
       list(
+        hierarchical_model = bernoulli_hierarchical_model,
         virus_G = bernoulli_bayes_model_sim_virus_G,
         experimental = bernoulli_bayes_model,
         experimental_full = bernoulli_bayes_model_v2
       ))
   ),
 
-  ## Model Diagnostics for Bayes Models  ###
+  #### Model Diagnostics for Real Bayes Models  ####
   tar_target(
     name = bernoulli_model_diagnostics,
     command = lapply(all_models,bayes_diagnostics)
   ),
 
-  ## Model Insights for Bayes Model trained on Simulation Virus AB ##
+  #### Model Insights ####
+
+  # Model Insights for Bayes Model trained on Simulation Virus AB ##
   tar_target(
     name = bernoulli_model_insights_sim_virus_AB,
     mapply(
@@ -206,7 +302,7 @@ list(
   ),
 
 
-  ## Model Insights for Bayes Model trained on Experimental data 1 ##
+  # Model Insights for Bayes Model trained on Experimental data 1 ##
   tar_target(
     name = bernoulli_model_insights,
     command = bayes_insights(
@@ -215,7 +311,7 @@ list(
       )
   ),
 
-  ## Model Insights for Bayes Model trained Combined data  ##
+  # Model Insights for Bayes Model trained Combined data  ##
   tar_target(
     name = bernoulli_model_insights_2,
     command = bayes_insights(
@@ -240,14 +336,26 @@ list(
   tar_target(
     name = report_data,
     command = list(
+      # Data Simulation checks
+      data_simulation_checks = list(
+        virusAB = simulation_data_check_AB,
+        virus_G = simulation_data_check_G,
+        simulation_data_check_hierarchical
+      ),
+      # Prior Predictive Simulations for the Real Models
+      prior_simulations = list(
+        prior_simulations = pp_simualtion_inspections),
+      # Models Diagnostics
       diagnostics = list(
         bernoulli_model_diagnostics
         ),
+      # Models insights
       insights = list(
         bernoulli_model_insights,
         bernoulli_model_insights_2,
         bernoulli_model_insights_sim_virus_AB
       ),
+      # Model Comparisons
       comparison = list(model_compare)
     )
   ),
@@ -260,5 +368,3 @@ list(
     params = list(data = report_data)
   )
 )
-
-
