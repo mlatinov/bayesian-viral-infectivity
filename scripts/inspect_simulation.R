e1034a7ba949ade114fa3a8baea0ac90e778c2f4
mlatinov <latinovmetodi@gmail.com>
2025-11-27 01:18:18 +0300

    remove stat plot and conditional effect

--- scripts/inspect_simulation.R
+++ scripts/inspect_simulation.R
@@ -16,20 +16,9 @@ inspect_simulation <- function(simulation) {
   theme_minimal()+
     ggtitle("Prior Predictive Simulation :Density plot")
 
-  stat <-
-    pp_check(simulation,type = "stat")+
-    theme_minimal()+
-    ggtitle("Prior Predictive Simulation : Stat")
-
-  # Conditional Effect of Virus simulation
-  conditional_effect <-
-    conditional_effects(simulation)
-
   # Return
   return(list(
     error_binned_plot = error_binned,
-    dens_overlay_plot = dens_overlay,
-    stat_plot = stat,
-    conditional_effect_plot = conditional_effect
+    dens_overlay_plot = dens_overlay
   ))
 }
