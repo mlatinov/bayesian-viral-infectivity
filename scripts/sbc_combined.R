268f9d02ac501dfc529d0997c5ae861545fcd0c7
mlatinov <latinovmetodi@gmail.com>
2025-11-28 00:24:28 +0300

    function to complete an SBC

--- scripts/sbc_combined.R
+++ scripts/sbc_combined.R
@@ -1,24 +1,44 @@
 
 #### Function for SBC on the Combined Model ####
-sbc_combined <- function(){
+sbc_combined <- function(brms_generator,n_sim = 100){
 
   #### Libraries ####
   library(SBC)
 
-  #### Generator ####
+  # Generate Datasets
+  simulated_datasets <- generate_datasets(generator = brms_generator, n_sims = n_sim)
 
-  #### Create SBC Datasets from generator ####
+  # SBC backend
+  backend <- SBC_backend_brms_from_generator(
+    generator = brms_generator,
+    chains = 4,
+    iter = 4000,
+    warmup = 2000
+    )
 
   #### Compute SBC ####
-  sbc <- compute_SBC(datasets = ,)
+  sbc <- compute_SBC(datasets = simulated_datasets, backend = backend)
 
+  #### Discovering bad parametrization ####
 
+  ## ECDF
+  ecdf <- plot_ecdf(sbc)+
+    theme_minimal()+
+    labs(
+      title = "Sampled ECDF Vs Theoretical CDF "
+    )
 
+  ## Coverage
+  coverage <- plot_coverage(sbc)+
+    theme_minimal()+
+    labs(
+      title = "Empirical coverage"
+    )
 
-
-
-
-
-
+  # Return
+  return(list(
+    ecdf = ecdf,
+    empirical_coverage = coverage
+  ))
 
 }
