a0eafd0320d962db4ab15bd39aa827ffde8b7a4e
mlatinov <latinovmetodi@gmail.com>
2025-12-05 01:13:33 +0300

    Update the extra plots with all sim models

--- scripts/extra_plots.R
+++ scripts/extra_plots.R
@@ -9,10 +9,16 @@ extra_model_plots <- function(model_list,model_insight_list){
   #### Extract Intercept for all models and mutate a marker ####
   beta_intercept_all <-
     bind_rows(
-      model_list$virus_low_infection %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "B"),
-      model_list$virus_not_effected %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "C"),
-      model_list$virus_resistant %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "D"),
-      model_list$virus_mutant %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "E")
+      model_list$b %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "B"),
+      model_list$c %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "C"),
+      model_list$d %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "D"),
+      model_list$e %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "E"),
+      model_list$f %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "F"),
+      model_list$g %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "G"),
+      model_list$h %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "H"),
+      model_list$i %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "I"),
+      model_list$j %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "J"),
+      model_list$k %>% spread_draws(b_Intercept,b_virus_dilution) %>% mutate(model = "K")
     )
 
   #### Plot posterior density in ridget plot for b_Intercept ####
@@ -47,9 +53,17 @@ extra_model_plots <- function(model_list,model_insight_list){
 
   #### EC50 ####
   ec_50 <- bind_rows(
-    model_insight_list$virus_low_infection$data$ec50_raw %>% mutate(model = "B"),
-    model_insight_list$virus_not_effected$data$ec50_raw %>% mutate(model = "C"),
-    model_insight_list$virus_mutant$data$ec50_raw %>% mutate(model = "E")
+    model_insight_list$b$data$ec50_raw %>% mutate(model = "B"),
+    model_insight_list$c$data$ec50_raw %>% mutate(model = "C"),
+    model_insight_list$d$data$ec50_raw %>% mutate(model = "D"),
+    model_insight_list$e$data$ec50_raw %>% mutate(model = "E"),
+    model_insight_list$f$data$ec50_raw %>% mutate(model = "F"),
+    model_insight_list$g$data$ec50_raw %>% mutate(model = "G"),
+    model_insight_list$h$data$ec50_raw %>% mutate(model = "H"),
+    model_insight_list$i$data$ec50_raw %>% mutate(model = "I"),
+    model_insight_list$j$data$ec50_raw %>% mutate(model = "J"),
+    model_insight_list$k$data$ec50_raw %>% mutate(model = "K")
+
   )
 
   ec_50$model <- paste0("theta[", ec_50$model, "]")
@@ -69,10 +83,16 @@ extra_model_plots <- function(model_list,model_insight_list){
 
   #### AUIPC ####
   auipc <- bind_rows(
-    model_insight_list$virus_low_infection$data$pauipc %>% mutate(model = "B"),
-    model_insight_list$virus_not_effected$data$pauipc %>% mutate(model = "C"),
-    model_insight_list$virus_resistant$data$pauipc %>% mutate(model = "D"),
-    model_insight_list$virus_mutant$data$pauipc %>% mutate(model = "E")
+    model_insight_list$b$data$pauipc %>% mutate(model = "B"),
+    model_insight_list$c$data$pauipc %>% mutate(model = "C"),
+    model_insight_list$d$data$pauipc %>% mutate(model = "D"),
+    model_insight_list$e$data$pauipc %>% mutate(model = "E"),
+    model_insight_list$f$data$pauipc %>% mutate(model = "F"),
+    model_insight_list$g$data$pauipc %>% mutate(model = "G"),
+    model_insight_list$h$data$pauipc %>% mutate(model = "H"),
+    model_insight_list$i$data$pauipc %>% mutate(model = "I"),
+    model_insight_list$j$data$pauipc %>% mutate(model = "J"),
+    model_insight_list$k$data$pauipc %>% mutate(model = "K")
   )
 
   auipc$model <- paste0("theta[", auipc$model, "]")
